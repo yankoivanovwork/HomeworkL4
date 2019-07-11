a1aed2b0b365e0e925361f29621ac9bd3ad52176
yankoivanovwork <51404578+yankoivanovwork@users.noreply.github.com>
2019-07-11 11:40:13 +0300

    Update Program.cs

--- HomeworkL4/Program.cs
+++ HomeworkL4/Program.cs
@@ -2,9 +2,9 @@
 
 namespace HomeworkL4
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             //zad1
             int.TryParse(Console.ReadLine(), out int n);
